9fdab6d449f7b2cc0949fe5cd9f3f948f1b31d37
GhunaymY <116463354+GhunaymY@users.noreply.github.com>
2023-09-11 17:37:53 -0600

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -2,6 +2,19 @@
 
 #Question 1: https://github.com/GhunaymY
 
-#Question 2: 
+#Python Script:
 import requests
 print(requests.__version__)
+print(requests.get(" http://www.google.com/"))
+
+#Question 2: The Version is 2.28.2
+
+#Question 3: The Version of the request Library in the virtualenv is 2.31.0
+
+#Question 4: The Version of the request Library without the virtualenv is 2.28.2
+
+#Question 5: The Status Code returned is HTTP/1.1 301. To get a status code of 200 you need to use the URL: www.google.ca
+
+#Question 6: The status code returned is: HTTP/1.1 418 I'm a Teapot. The one returned by -i. 
+
+#Question 7: 
